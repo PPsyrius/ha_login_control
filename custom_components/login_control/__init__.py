98c98a1444fa07b36d6afc06045b7a8b33a4c7f0
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2022-05-25 03:15:58 +0700

    Initial commit

---
+++ custom_components/login_control/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+DOMAIN = 'login_control'
+
+async def async_setup(hass, config):
+    """Async setup is called when the service got called."""
+
+    async def handle_refresh_token_clear(call):
+        """Handles the service call."""
+        user_id = call.data.get("user_id")
+        user = await hass.auth.async_get_user(user_id)
+
+        tokens = list(user.refresh_tokens.values())
+
+        for token in tokens:
+            await hass.auth.async_remove_refresh_token(token)
+
+    hass.services.async_register(DOMAIN, 'clear_refresh_tokens', handle_refresh_token_clear)
+
+    # Return boolean to indicate that initialization was successful.
+    return True
